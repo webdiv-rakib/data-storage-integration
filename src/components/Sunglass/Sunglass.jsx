7517aa780fd36d48489b5dc725365b66762ef512
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-02-05 23:21:51 +0600

    ES6 Modules, import default, relative path

--- src/components/Sunglass/Sunglass.jsx
+++ src/components/Sunglass/Sunglass.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import './Sunglass.css'
+import Watch from '../Watch/Watch';
+import { add, divideTheFirstNumberByTheSecondNumber as divide, multiply } from '../../Utilities/calculate';
+// import add from '../../Utilities/calculate';
 
 const Sunglass = () => {
+    const first = 55;
+    const second = 199;
+    const sum = add(first, second);
+    const multi = multiply(first, second);
+    const vaag = divide(first, second);
     return (
         <div>
-
+            <Watch></Watch>
         </div>
     );
 };
